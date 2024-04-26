2dfea80bcf244b78bbcd2a991f881df906811c99
Burak Şenses <buraksenseees@gmail.com>
2024-04-26 17:13:41 +0300

    -add category validators

---
+++ EcoVerse.ProductManagement.Application/Validations/Category/CreateCategoryValidator.cs
@@ -0,0 +1,18 @@
+﻿using EcoVerse.ProductManagement.Application.DTOs.Category;
+using FluentValidation;
+
+namespace EcoVerse.ProductManagement.Application.Validations.Category;
+
+public class CreateCategoryValidator : AbstractValidator<CreateCategoryDto>
+{
+    public CreateCategoryValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .Length(3, 100).WithMessage("Name must be between 3 and 100 characters.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .Length(10, 100).WithMessage("Description must be between 3 and 100 characters.");
+    }
+}
