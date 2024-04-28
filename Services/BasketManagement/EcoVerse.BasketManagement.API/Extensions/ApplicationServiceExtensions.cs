88ec044e185e83ae806d3e3ddb546118191503bf
Burak Şenses <buraksenseees@gmail.com>
2024-04-28 14:28:08 +0300

    -add IserviceCollection configs moved to another class

---
+++ Services/BasketManagement/EcoVerse.BasketManagement.API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using EcoVerse.BasketManagement.Application.Interfaces;
+using EcoVerse.BasketManagement.Application.Services;
+using EcoVerse.BasketManagement.Domain.Interfaces;
+using EcoVerse.BasketManagement.Infrastructure.Repositories;
+using EcoVerse.BasketManagement.Infrastructure.Services;
+using EcoVerse.BasketManagement.Infrastructure.Settings;
+using EcoVerse.Shared.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace EcoVerse.BasketManagement.API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+        JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+        services.AddControllers(opt =>
+        {
+            opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+        });
+        
+        services.AddControllers(opt =>
+        {
+            opt.Filters.Add(new AuthorizeFilter());
+        });
+        
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.Configure<RedisSettings>(config.GetSection("RedisSettings"));
+        services.AddSingleton<RedisService>(sp =>
+        {
+            var redisSettings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+            var redis = new RedisService(redisSettings.Host, redisSettings.Port);
+            redis.Connect();
+            return redis;
+        });
+
+        services.AddHttpContextAccessor();
+        services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+        services.AddScoped<ICartRepository, CartRepository>();
+        services.AddScoped<ICartService, CartService>();
+        
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+        {
+            options.Authority = config["IdentityServerURL"];
+            options.Audience = "resource_catalog";
+            options.RequireHttpsMetadata = false;
+        });
+
+        return services;
+    }
+}
--- Services/BasketManagement/EcoVerse.BasketManagement.API/Program.cs
+++ Services/BasketManagement/EcoVerse.BasketManagement.API/Program.cs
@@ -1,49 +1,9 @@
-using System.IdentityModel.Tokens.Jwt;
-using EcoVerse.BasketManagement.Application.Interfaces;
-using EcoVerse.BasketManagement.Application.Services;
-using EcoVerse.BasketManagement.Domain.Interfaces;
-using EcoVerse.BasketManagement.Infrastructure.Repositories;
-using EcoVerse.BasketManagement.Infrastructure.Services;
-using EcoVerse.BasketManagement.Infrastructure.Settings;
-using EcoVerse.Shared.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.Extensions.Options;
+using EcoVerse.BasketManagement.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
-builder.Services.AddControllers(opt =>
-{
-    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
-});
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
-builder.Services.AddSingleton<RedisService>(sp =>
-{
-    var redisSettings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
-    var redis = new RedisService(redisSettings.Host, redisSettings.Port);
-    redis.Connect();
-    return redis;
-});
-
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
-builder.Services.AddScoped<ICartRepository, CartRepository>();
-builder.Services.AddScoped<ICartService, CartService>();
-        
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.Authority = builder.Configuration["IdentityServerURL"];
-    options.Audience = "resource_basket";
-    options.RequireHttpsMetadata = false;
-});
-
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
